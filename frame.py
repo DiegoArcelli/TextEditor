cfa1c58dbb999a176f47e6abfe63974a78b93bed
root <root@diego.diego>
2019-10-28 20:26:14 +0100

    exit

--- frame.py
+++ frame.py
@@ -1,10 +1,12 @@
 from Tkinter import *
 from tkFileDialog import *
-from dialog import *
 
 class MainFrame:
 
+    root = None
+
     def __init__(self, root):
+        self.root = root
         textFrame = Frame(root)
         scroll = Scrollbar(textFrame)
         scroll.pack(side=RIGHT, fill=Y)
@@ -17,7 +19,7 @@ class MainFrame:
         filemenu.add_command(label="Open", command=lambda: self.open_file(textArea))
         filemenu.add_command(label="Save", command=lambda: self.save_file(textArea))
         filemenu.add_separator()
-        filemenu.add_command(label="Exit", command=root.quit)
+        filemenu.add_command(label="Exit", command=lambda: self.exit(textFrame))
         menubar.add_cascade(label="File", menu=filemenu)
         editmenu = Menu(menubar, tearoff=0)
         editmenu.add_command(label="Copy")
@@ -27,6 +29,10 @@ class MainFrame:
         helpmenu = Menu(menubar, tearoff=0)
         helpmenu.add_command(label="About", command=lambda: self.about_window(textFrame))
         menubar.add_cascade(label="Help", menu=helpmenu)
+        viewmenu = Menu(menubar, tearoff=0)
+        viewmenu.add_command(label="Zoom +", command=lambda: self.increaseTextSize(textFrame))
+        viewmenu.add_command(label="Zoom -")
+        menubar.add_cascade(label="View", menu=viewmenu)
         root.config(menu=menubar)
 
     def save_file(self,text):
@@ -46,5 +52,22 @@ class MainFrame:
         text.insert(INSERT,data)
 
     def about_window(self,parent):
-        d = Dialog(parent)
-        parent.wait_window(d.top)
+        top = self.top = Toplevel(parent)
+        top.title("Exit")
+        file = open("about","r")
+        data = file.read()
+        l = Label(top, text=data)
+        l.pack(pady=5,padx=5)
+        parent.wait_window(top)
+
+
+    def exit(self, parent):
+        top = self.top = Toplevel(parent)
+        top.title("Exit")
+        l = Label(top, text="Are you sure?")
+        l.pack(pady=5,padx=5)
+        b1 = Button(top, text="Yes", command=self.root.destroy)
+        b1.pack(pady=5)
+        b2 = Button(top, text="No", command=top.destroy)
+        b2.pack(pady=5)
+        parent.wait_window(top)
