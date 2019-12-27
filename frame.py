fa7a4be1b9850fa477e4c0ba477c64162f0248d2
root <root@diego.diego>
2019-12-27 22:09:12 +0100

    copy-paste

--- frame.py
+++ frame.py
@@ -16,7 +16,7 @@ class MainFrame:
         textFrame = Frame(root)
         scroll = Scrollbar(textFrame)
         scroll.pack(side=RIGHT, fill=Y)
-        textArea = Text(textFrame,yscrollcommand=scroll.set,font=("Ubuntu", fontSize))
+        textArea = Text(textFrame,yscrollcommand=scroll.set,font=("Helvetica", fontSize))
         scroll.config(command=textArea.yview)
         textFrame.pack(fill='both', expand='yes')
         self.textArea = textArea
@@ -29,22 +29,19 @@ class MainFrame:
         filemenu.add_command(label="Exit", command=lambda: self.exit(textFrame))
         menubar.add_cascade(label="File", menu=filemenu)
         editmenu = Menu(menubar, tearoff=0)
-        editmenu.add_command(label="Copy")
-        editmenu.add_command(label="Cut")
-        editmenu.add_command(label="Paste")
+        editmenu.add_command(label="Copy", command=lambda: self.textArea.event_generate("<<Copy>>"))
+        editmenu.add_command(label="Cut", command=lambda: self.textArea.event_generate("<<Cut>>"))
+        editmenu.add_command(label="Paste", command=lambda: self.textArea.event_generate("<<Paste>>"))
         menubar.add_cascade(label="Edit", menu=editmenu)
         helpmenu = Menu(menubar, tearoff=0)
         helpmenu.add_command(label="About", command=lambda: self.about_window(textFrame))
+        helpmenu.add_command(label="Shortcuts", command=lambda: self.shortcuts_window(textFrame))
         menubar.add_cascade(label="Help", menu=helpmenu)
         viewmenu = Menu(menubar, tearoff=0)
         viewmenu.add_command(label="Zoom +", command=lambda: self.changeTextSize(textFrame,scroll,1))
         viewmenu.add_command(label="Zoom -", command=lambda: self.changeTextSize(textFrame,scroll,-1))
         menubar.add_cascade(label="View", menu=viewmenu)
-        #menubar.config(font=("Ubuntu ",8))
         root.config(menu=menubar)
-        '''root.bind('<Control-c>', lambda e: self.copy())
-        root.bind('<Control-x>', lambda e: self.cut())
-        root.bind('<Control-v>', lambda e: self.paste())'''
         root.bind('<Control-o>', lambda e: self.open_file(textArea))
         root.bind('<Control-O>', lambda e: self.open_file(textArea))
         root.bind('<Control-s>', lambda e: self.save_file(textArea))
@@ -55,6 +52,10 @@ class MainFrame:
         root.bind('<Control-A>', lambda e: self.select_all(textArea))
         root.bind('<Control-e>', lambda e: self.exit(textFrame))
         root.bind('<Control-E>', lambda e: self.exit(textFrame))
+        root.bind('<Control-H>', lambda e: self.about_window(textFrame))
+        root.bind('<Control-h>', lambda e: self.about_window(textFrame))
+        root.bind('<Control-l>', lambda e: self.shortcuts_window(textFrame))
+        root.bind('<Control-L>', lambda e: self.shortcuts_window(textFrame))
 
     def save_file(self,text):
         if self.openedFile == None:
@@ -82,40 +83,37 @@ class MainFrame:
 
     def about_window(self,parent):
         top = self.top = Toplevel(parent)
-        top.title("Exit")
+        top.title("About")
+        top.resizable(width=False, height=False)
         file = open("about","r")
         data = file.read()
         l = Label(top, text=data)
-        l.pack()
-        url = Label(top, text="GitHub Repository", fg="blue", cursor="hand2")
-        url.pack()
+        l.grid(row = 0, pady = (5,0), padx = (5,5))
+        url = Label(top, text="GitHub Repository", fg="blue", cursor="hand2", height=5, width=20)
+        url.grid(row = 1, pady = (0,0), padx = (0,0))
         url.bind("<Button-1>", lambda x: webbrowser.open_new("https://github.com/DiegoArcelli/TextEditor"))
         parent.wait_window(top)
+
+    def shortcuts_window(self,parent):
+        top = self.top = Toplevel(parent)
+        top.title("Shortcuts")
+        file = open("shorts","r")
+        data = file.read()
+        l = Label(top, text=data, anchor = SW, justify = LEFT, padx = 20, pady = 15)
+        l.pack()
+        top.resizable(width=False, height=False)
 
     def exit(self, parent):
         top = self.top = Toplevel(parent)
         top.title("Exit")
+        top.resizable(width=False, height=False)
         l = Label(top, text="Are you sure?")
-        l.pack(pady=5,padx=5)
+        l.grid(row = 0, columnspan = 2, pady = (10,10), padx = (10,10))
         b1 = Button(top, text="Yes", command=self.root.destroy)
-        b1.pack(pady=5)
+        b1.grid(column = 0, row = 1, pady = (10,10), padx = (10,10))
         b2 = Button(top, text="No", command=top.destroy)
-        b2.pack(pady=5)
+        b2.grid(column = 1, row = 1, pady = (10,10), padx = (10,10))
         parent.wait_window(top)
-
-    def copy(self, text):
-        self.clipboard_clear()
-        text = self.get("sel.first", "sel.last")
-        self.clipboard_append(text)
-
-
-    def cut(self, event):
-        self.copy()
-        self.delete("sel.first", "sel.last")
-
-    def paste(self, event):
-        text = self.selection_get(selection='CLIPBOARD')
-        self.insert('insert', text)
 
     def select_all(self, text):
         text.tag_add(SEL, "1.0", END)
@@ -126,7 +124,7 @@ class MainFrame:
         text = self.textArea.get("1.0",END)
         self.textArea.destroy()
         self.fontSize+=val
-        self.textArea = Text(parent,yscrollcommand=scroll.set, font=("Ubuntu", self.fontSize))
+        self.textArea = Text(parent, yscrollcommand=scroll.set, font=("Helvetica", self.fontSize))
         parent.pack(fill='both', expand='yes')
         parent.pack(fill='both', expand='yes')
         self.textArea.insert(END,text)
